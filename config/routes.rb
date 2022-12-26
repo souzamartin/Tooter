d5e6f917004254f3ca5aa585fa37b02c63a4470a
Martin <souzam@sonic.net>
2022-12-26 12:17:35 -0500

    Namespaced API routes, created fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :toot_tags
-  resources :tags
-  resources :toots
-  resources :users, only: [:create, :show, :destroy]
+  namespace :api do
+    resources :toot_tags
+    resources :tags
+    resources :toots
+    resources :users, only: [:create, :show, :destroy]
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/me', to: 'users#check'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    get '/me', to: 'users#check'
+  end
+
+  get '*path', to: 'fallback#index', constraints: ->(req) {!req.xhr? && req.format.html?} 
   
 end
