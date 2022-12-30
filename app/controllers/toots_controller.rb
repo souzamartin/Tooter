53b18fa3b5b942cfee2582a0c3c29a5e194f5484
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-12-30 18:27:38 -0500

    Completed backend for like increment

--- app/controllers/toots_controller.rb
+++ app/controllers/toots_controller.rb
@@ -1,5 +1,6 @@
 class TootsController < ApplicationController
     skip_before_action :authorize, only: [:index]
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
     def index 
         render json: Toot.all.order(created_at: :desc), status: :ok
@@ -9,5 +10,17 @@ class TootsController < ApplicationController
         toot = Toot.find(params[:id])
         toot.destroy 
         head :no_content
+    end
+
+    def increment_likes
+        toot = Toot.find(params[:id])
+        toot.update(likes: toot.likes + 1)
+        render json: toot
+    end
+
+    private 
+
+    def not_found_response
+        render json: {error: "Toots not found"}, status: :not_found
     end
 end
