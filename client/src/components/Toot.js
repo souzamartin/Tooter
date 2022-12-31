2666f596fb13c9e963a6f81785f702ed974449cc
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-12-31 01:27:44 -0500

    Completed(kinda) frontend for like btn

--- client/src/components/Toot.js
+++ client/src/components/Toot.js
@@ -1,23 +1,41 @@
-const Toot = ({toot, user, deleteToot}) => {
-
+const Toot = ({ toot, user, deleteToot }) => {
   const handleDelete = () => {
     deleteToot(toot.id);
   };
 
-  const renderedTags = toot.tag_labels.map((tag, index) => {return <span key={index} className="tag">{tag}</span>})
+  const renderedTags = toot.tag_labels.map((tag, index) => {
+    return (
+      <span key={index} className="tag">
+        {tag}
+      </span>
+    );
+  });
 
-  const timestamp = new Date(toot.created_at)
+  const timestamp = new Date(toot.created_at);
+
+  const handleLike = () => {
+    fetch(`/toots/${toot.id}/like`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((r) => r.json())
+      .then(window.location.reload(true));
+  };
 
   return (
     <div id="toot-card">
       <h3>{toot.user.username}</h3>
       <p>{toot.content}</p>
       {renderedTags}
-      <p>💕 {toot.likes}</p>
+      <p onClick={handleLike}>💕 {toot.likes}</p>
       {user.id === toot.user.id ? (
         <button onClick={handleDelete}>❌</button>
       ) : null}
-      <span className="timestamp">{timestamp.toLocaleString('en-us', {timeZone: 'EST'})}</span>
+      <span className="timestamp">
+        {timestamp.toLocaleString("en-us", { timeZone: "EST" })}
+      </span>
     </div>
   );
 };
