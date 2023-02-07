9bca3a7fea77bc36435b1593fe2ce5a2221b1549
Martin <souzam@sonic.net>
2023-02-07 11:39:11 -0500

    Updated seedfile

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
 # README
 
-*TOOTER*
+## *TOOTER*
 
-Originally a phase 4 project by Jonny Tilahun & Martin Souza
-
-Flatiron School Software Engineering program
-
-10/10/22 east cohort
-
+Originally a phase 4 project by Jonny Tilahun & Martin Souza  
+Flatiron School Software Engineering program  
+10/10/22 east cohort  
 This fork updated and maintained by Martin Souza
 
-
 ## ABOUT
-Tooter is a cheeky social media app where users can share their hottest takes in 25 characters or less, and categorize posts with associated tags.
+Tooter is a cheeky social media app where users can share their spiciest or silliest takes in 25 characters or less, and categorize posts with associated tags.
 
+### Users can:
+• Create an account and log in/out; update and delete their account  
+• View all users' toots; view only an individual user's toots; filter toots by their tags  
+• Add likes to posted toots  
+• Post new toots and add tags  
+• Delete their own toots
 
 ## TECHNOLOGIES
-• SQLite relational database
-
-• Rails API with Active Record and Active Model Serializers
-
-• User auth with BCrypt
-
-• React client with custom CSS
+• SQLite relational database  
+• Rails API with Active Record and Active Model Serializers  
+• User auth with BCrypt  
+• React frontend with custom CSS
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,31 @@ Toot.destroy_all
 Tag.destroy_all
 TootTag.destroy_all
 
-u1 = User.create(username: 'Dragonmaster Adam', password: 'password', email: 'adam@flatiron.com', avatar_img: 'temp')
-u2 = User.create(username: 'Wizard Sam', password: 'password', email: 'sam@flatiron.com', avatar_img: 'temp')
+u1 = User.create(username: 'Dragonmaster Adam', password: 'password', email: 'adam@flatiron.com')
+u2 = User.create(username: 'Wizard Sam', password: 'password', email: 'sam@flatiron.com')
+u3 = User.create(username: 'Sorcerer Ix', password: 'password', email: 'ix@flatiron.com')
 
-toot1 = Toot.create(content: 'potato', likes: 10, user_id: u1.id)
-toot2 = Toot.create(content: 'meow', likes: 2, user_id: u1.id)
-toot3 = Toot.create(content: 'wowzer', likes: 7, user_id: u1.id)
+toot0 = Toot.create(content: 'Rose is the best', likes: 18, user_id: u3.id)
+toot1 = Toot.create(content: 'vim is the best', likes: 10, user_id: u1.id)
+toot2 = Toot.create(content: 'cat on keyboard!!', likes: 9, user_id: u1.id)
+toot3 = Toot.create(content: 'thinking about vhs store', likes: 0, user_id: u1.id)
 toot4 = Toot.create(content: 'U MADE IT THRU', likes: 9000, user_id: u2.id)
 toot5 = Toot.create(content: 'ANOTHER DAY', likes: 9001, user_id: u2.id)
+toot6 = Toot.create(content: 'Rails rules, don\'t @ me', likes: 22, user_id: u3.id)
 
-tag1 = Tag.create(tag_text: 'vegi')
-tag2 = Tag.create(tag_text: 'animal')
+tag1 = Tag.create(tag_text: 'code stuff')
+tag2 = Tag.create(tag_text: 'cats')
 tag3 = Tag.create(tag_text: 'random')
+tag4 = Tag.create(tag_text: 'catastrophe')
 
 TootTag.create(toot_id: toot1.id, tag_id: tag1.id)
-TootTag.create(toot_id: toot1.id, tag_id: tag3.id)
 TootTag.create(toot_id: toot2.id, tag_id: tag2.id)
 TootTag.create(toot_id: toot2.id, tag_id: tag3.id)
-TootTag.create(toot_id: toot3.id, tag_id: tag3.id)
+TootTag.create(toot_id: toot2.id, tag_id: tag4.id)
+TootTag.create(toot_id: toot3.id, tag_id: tag1.id)
 TootTag.create(toot_id: toot4.id, tag_id: tag3.id)
 TootTag.create(toot_id: toot5.id, tag_id: tag3.id)
+TootTag.create(toot_id: toot0.id, tag_id: tag2.id)
+TootTag.create(toot_id: toot6.id, tag_id: tag1.id)
 
 puts "Seeding complete"
